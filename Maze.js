bdff65798c62947979f2dadd552664d93ff9e44f
jryla <02justin.ryland@gulllakecs.org>
2025-01-24 08:17:24 -0500

    Create Maze.js

---
+++ Maze.js
@@ -0,0 +1,82 @@
+//Team: Justin Ryland & Andrew Caruso
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-B0D0
+
+//Start
+async function startProgram() {
+
+
+//Start & Say Start
+//Say Start and print Start
+//Driver: Andrew Caruso
+//Navigator: Justin Ryland
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Justin Ryland
+//Navigator: Andrew Caruso
+
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt Makes Sound of Choice - Update Sound
+//Driver: Andrew Caruso
+//Navigator: Justin Ryland
+
+
+
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Justin Ryland
+//Navigator: Andrew Caruso
+
+
+
+
+
+//Sound #2 Checkpoint
+//Bolt Makes Sound of Choice - Update Sound
+//Driver: Andrew Caruso
+//Navigator: Justin Ryland
+
+
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Justin Ryland
+//Navigator: Andrew Caruso
+
+
+
+//Sound #3 Checkpoint
+//Bolt Makes Sound of Choice - Update Sound
+//Driver: Andrew Caruso
+//Navigator: Justin Ryland
+
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver: Justin Ryland
+//Navigator: Andrew Caruso
+
+
+
+
+//Finished & say Finished
+//Say Finished, and say it aloud
+//Driver: Andrew Caruso
+//Navigator: Justin Ryland
+
+
+//End Program
+}
